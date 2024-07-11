fbc4f9eb6b45ecc4575cef3b84f7b330e66127c8
이정욱 <65989344+ukly@users.noreply.github.com>
2024-07-11 17:39:48 +0900

    [FEAT] Create Project

---
+++ src/main/java/bootcamp/geulhyang/GeulhyangApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.geulhyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeulhyangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeulhyangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp/geulhyang/GeulhyangApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.geulhyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeulhyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

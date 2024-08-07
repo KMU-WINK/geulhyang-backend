99a1e6ddc3d093dc816ccdc1480e968458a92ded
이정욱 <65989344+ukly@users.noreply.github.com>
2024-08-07 19:07:40 +0900

    [FEAT] JWT 토큰을 통한 미들웨어 생성

---
+++ src/main/java/bootcamp/geulhyang/security/JwtAuthenticationFilter.java
@@ -0,0 +1,71 @@
+package bootcamp.geulhyang.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public JwtAuthenticationFilter(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException{
+        String path = request.getRequestURI();
+        if (path.startsWith("/auth")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            // 토큰 검증 로직 추가
+
+            try {
+                Claims claims = Jwts.parserBuilder()
+                        .setSigningKey(secretKey)
+                        .build()
+                        .parseClaimsJws(token)
+                        .getBody();
+
+                String nickname = claims.getSubject();
+                Long userId = claims.get("userId", Long.class);
+
+                if (nickname != null && userId != null) {
+                    UserDetails userDetails = new User(nickname, "", Collections.emptyList());
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+
+            } catch (JwtException e) {
+
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- src/main/java/bootcamp/geulhyang/security/SecurityConfig.java
+++ src/main/java/bootcamp/geulhyang/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package bootcamp.geulhyang.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,7 +26,8 @@ public class SecurityConfig {
                                 .requestMatchers("/auth/*").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(formLogin -> formLogin.disable()); // 폼 로그인을 비활성화
+                .formLogin(formLogin -> formLogin.disable()) // 폼 로그인을 비활성화
+                .addFilterBefore(new JwtAuthenticationFilter(secretKey), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
